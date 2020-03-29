96bde0839cf119e46771d856d919d66d23515d7a
Mourad Bougarne <contact@mbougarne.me>
2020-03-29 16:07:07 +0200

    Start User Controller Testing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Repository\Contracts\CrudRepositoryInterface;
 
 class UserController extends Controller
 {
+    protected Request $request;
+    protected CrudRepositoryInterface $repository;
+
+    public function __construct(Request $request, CrudRepositoryInterface $repository)
+    {
+        $this->request = $request;
+        $this->repository = $repository;
+    }
+
     /**
      * Show the form for creating a new resource.
      *
@@ -25,7 +35,14 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $userCreation = $this->repository->save($this->validateRequest());
+
+        if($userCreation)
+        {
+            return response()->json([$userCreation], 200);
+        }
+
+        return response()->json(['message' => "There is an error"], 500);
     }
 
     /**
@@ -38,5 +55,14 @@ class UserController extends Controller
     public function update(Request $request, User $user)
     {
         //
+    }
+
+    protected function validateRequest()
+    {
+        return $this->request->validate([
+            'username' => 'required|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
     }
 }
