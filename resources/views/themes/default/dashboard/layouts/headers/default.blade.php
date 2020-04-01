a4a7d9e1b6f2c549f74e5604daed22a2bd57d65b
Mourad Bougarne <contact@mbougarne.me>
2020-04-01 14:18:45 +0100

    Add Stisla assets files

---
+++ resources/views/themes/default/dashboard/layouts/headers/default.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Ecommerce Dashboard &mdash; Stisla</title>
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/components.css">
+</head>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper">
