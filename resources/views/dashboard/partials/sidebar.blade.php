7b7a85449f5e3bd18f69d7272ec85814fc9626bd
Mourad Bougarne <contact@mbougarne.me>
2020-04-03 15:54:29 +0100

    Setup the sidebar

--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -1,154 +1,181 @@
 {{-- Sidebar / Dashboard Navigation --}}
 <div class="main-sidebar">
     <aside id="sidebar-wrapper">
-        <div class="sidebar-brand sidebar-gone-show"><a href="index.html">Stisla</a></div>
+        {{-- Logo --}}
+        <div class="sidebar-brand sidebar-gone-show">
+            <a href="{{ route('dashboard') }}">
+                @if(isset($settings->logo) && !empty($settings->logo))
+                    <img
+                        src="{{ asset('uploads/settings/' . $settings->logo) }}"
+                        alt="{{ $settings->title }}">
+                @else
+                    {{ env('APP_NAME') }}
+                @endif
+            </a>
+        </div>
+        {{-- Navigation Links --}}
         <ul class="sidebar-menu">
-            <li class="menu-header">Dashboard</li>
+            {{-- Dashboard --}}
+            <li class="menu-header">{{ __("Dashboard") }}</li>
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('dashboard') }}">
+                    <i class="fas fa-fire"></i>
+                    <span>{{ __("Dashboard") }}</span>
+                </a>
+            </li>
+            {{-- Content --}}
+            <li class="menu-header">{{ __("Content") }}</li>
             <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-            </ul>
+                <a
+                    href="#"
+                    class="nav-link has-dropdown"
+                    data-toggle="dropdown">
+                    <i class="fas fa-newspaper"></i>
+                    <span>{{ __("Posts") }}</span>
+                </a>
+                <ul class="dropdown-menu">
+                    {{-- All posts --}}
+                    <li>
+                        <a
+                            class="nav-link"
+                            href="{{ route('posts') }}">
+                            {{ __("Posts") }}
+                        </a>
+                    </li>
+                    {{-- Create Post --}}
+                    <li>
+                        <a
+                            class="nav-link"
+                            href="{{ route('post.create') }}">
+                            {{ __("Create Post") }}
+                        </a>
+                    </li>
+                    {{-- Categories --}}
+                    <li>
+                        <a
+                            class="nav-link"
+                            href="{{ route('categories') }}">
+                            {{ __("Categories") }}
+                        </a>
+                    </li>
+                    {{-- Tags --}}
+                    <li>
+                        <a
+                            class="nav-link"
+                            href="{{ route('tags') }}">
+                            {{ __("Tags") }}
+                        </a>
+                    </li>
+                </ul>
             </li>
-            <li class="menu-header">Starter</li>
-            <li class="nav-item dropdown active">
-            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
-                <li class="active"><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
-                <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
-            </ul>
+            {{-- Pages --}}
+            <li class="nav-item">
+                <a
+                    href="#"
+                    class="nav-link has-dropdown"
+                    data-toggle="dropdown">
+                    <i class="fas fa-poll-h"></i>
+                    <span>{{ __("Pages") }}</span>
+                </a>
+                <ul class="dropdown-menu">
+                    {{-- All pages --}}
+                    <li>
+                        <a
+                            class="nav-link"
+                            href="{{ route('pages') }}">
+                            {{ __("Pages") }}
+                        </a>
+                    </li>
+                    {{-- Create Page --}}
+                    <li>
+                        <a
+                            class="nav-link"
+                            href="{{ route('page.create') }}">
+                            {{ __("Create Page") }}
+                        </a>
+                    </li>
+                </ul>
             </li>
-            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
-            <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
-                <li><a class="nav-link" href="bootstrap-badge.html">Badge</a></li>
-                <li><a class="nav-link" href="bootstrap-breadcrumb.html">Breadcrumb</a></li>
-                <li><a class="nav-link" href="bootstrap-buttons.html">Buttons</a></li>
-                <li><a class="nav-link" href="bootstrap-card.html">Card</a></li>
-                <li><a class="nav-link" href="bootstrap-carousel.html">Carousel</a></li>
-                <li><a class="nav-link" href="bootstrap-collapse.html">Collapse</a></li>
-                <li><a class="nav-link" href="bootstrap-dropdown.html">Dropdown</a></li>
-                <li><a class="nav-link" href="bootstrap-form.html">Form</a></li>
-                <li><a class="nav-link" href="bootstrap-list-group.html">List Group</a></li>
-                <li><a class="nav-link" href="bootstrap-media-object.html">Media Object</a></li>
-                <li><a class="nav-link" href="bootstrap-modal.html">Modal</a></li>
-                <li><a class="nav-link" href="bootstrap-nav.html">Nav</a></li>
-                <li><a class="nav-link" href="bootstrap-navbar.html">Navbar</a></li>
-                <li><a class="nav-link" href="bootstrap-pagination.html">Pagination</a></li>
-                <li><a class="nav-link" href="bootstrap-popover.html">Popover</a></li>
-                <li><a class="nav-link" href="bootstrap-progress.html">Progress</a></li>
-                <li><a class="nav-link" href="bootstrap-table.html">Table</a></li>
-                <li><a class="nav-link" href="bootstrap-tooltip.html">Tooltip</a></li>
-                <li><a class="nav-link" href="bootstrap-typography.html">Typography</a></li>
-            </ul>
+            {{-- Comments --}}
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('comments') }}">
+                    <i class="fas fa-fire"></i>
+                    <span>{{ __("Comments") }}</span>
+                </a>
             </li>
-            <li class="menu-header">Stisla</li>
-            <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Components</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="components-article.html">Article</a></li>
-                <li><a class="nav-link beep beep-sidebar" href="components-avatar.html">Avatar</a></li>
-                <li><a class="nav-link" href="components-chat-box.html">Chat Box</a></li>
-                <li><a class="nav-link beep beep-sidebar" href="components-empty-state.html">Empty State</a></li>
-                <li><a class="nav-link" href="components-gallery.html">Gallery</a></li>
-                <li><a class="nav-link beep beep-sidebar" href="components-hero.html">Hero</a></li>
-                <li><a class="nav-link" href="components-multiple-upload.html">Multiple Upload</a></li>
-                <li><a class="nav-link beep beep-sidebar" href="components-pricing.html">Pricing</a></li>
-                <li><a class="nav-link" href="components-statistic.html">Statistic</a></li>
-                <li><a class="nav-link" href="components-tab.html">Tab</a></li>
-                <li><a class="nav-link" href="components-table.html">Table</a></li>
-                <li><a class="nav-link" href="components-user.html">User</a></li>
-                <li><a class="nav-link beep beep-sidebar" href="components-wizard.html">Wizard</a></li>
-            </ul>
+            {{-- Contacts --}}
+            <li class="menu-header">{{ __("Contacts") }}</li>
+            <li class="nav-item">
+                <a
+                    href="#"
+                    class="nav-link has-dropdown"
+                    data-toggle="dropdown">
+                    <i class="fas fa-address-book"></i>
+                    <span>{{ __("Contacts") }}</span>
+                </a>
+                <ul class="dropdown-menu">
+                    {{-- All Contacts --}}
+                    <li>
+                        <a
+                            class="nav-link"
+                            href="{{ route('contacts') }}">
+                            {{ __("Contacts") }}
+                        </a>
+                    </li>
+                    {{-- Create Contact --}}
+                    <li>
+                        <a
+                            class="nav-link"
+                            href="{{ route('contact.create') }}">
+                            {{ __("Create Contact") }}
+                        </a>
+                    </li>
+                    {{-- Forms --}}
+                    <li>
+                        <a
+                            class="nav-link"
+                            href="{{ route('forms') }}">
+                            {{ __("Forms") }}
+                        </a>
+                    </li>
+                    {{-- Create Form --}}
+                    <li>
+                        <a
+                            class="nav-link"
+                            href="{{ route('form.create') }}">
+                            {{ __("Create Form") }}
+                        </a>
+                    </li>
+                </ul>
             </li>
-            <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Forms</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
-                <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
-                <li><a class="nav-link" href="forms-validation.html">Validation</a></li>
-            </ul>
+            {{-- Settings --}}
+            <li class="menu-header">{{ __("Settings") }}</li>
+            <li class="nav-item">
+                {{-- Settings --}}
+                <a
+                    class="nav-link"
+                    href="{{ route('settings.update') }}">
+                    <i class="fas fa-cogs"></i>
+                    {{ __("Settings") }}
+                </a>
             </li>
-            <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-map-marker-alt"></i> <span>Google Maps</span></a>
-            <ul class="dropdown-menu">
-                <li><a href="gmaps-advanced-route.html">Advanced Route</a></li>
-                <li><a href="gmaps-draggable-marker.html">Draggable Marker</a></li>
-                <li><a href="gmaps-geocoding.html">Geocoding</a></li>
-                <li><a href="gmaps-geolocation.html">Geolocation</a></li>
-                <li><a href="gmaps-marker.html">Marker</a></li>
-                <li><a href="gmaps-multiple-marker.html">Multiple Marker</a></li>
-                <li><a href="gmaps-route.html">Route</a></li>
-                <li><a href="gmaps-simple.html">Simple</a></li>
-            </ul>
+            <li class="nav-item">
+                {{-- Menus --}}
+                <a
+                    class="nav-link"
+                    href="{{ route('menus') }}">
+                    <i class="fas fa-list"></i>
+                    {{ __("Menus") }}
+                </a>
             </li>
-            <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i> <span>Modules</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="modules-calendar.html">Calendar</a></li>
-                <li><a class="nav-link" href="modules-chartjs.html">ChartJS</a></li>
-                <li><a class="nav-link" href="modules-datatables.html">DataTables</a></li>
-                <li><a class="nav-link" href="modules-flag.html">Flag</a></li>
-                <li><a class="nav-link" href="modules-font-awesome.html">Font Awesome</a></li>
-                <li><a class="nav-link" href="modules-ion-icons.html">Ion Icons</a></li>
-                <li><a class="nav-link" href="modules-owl-carousel.html">Owl Carousel</a></li>
-                <li><a class="nav-link" href="modules-sparkline.html">Sparkline</a></li>
-                <li><a class="nav-link" href="modules-sweet-alert.html">Sweet Alert</a></li>
-                <li><a class="nav-link" href="modules-toastr.html">Toastr</a></li>
-                <li><a class="nav-link" href="modules-vector-map.html">Vector Map</a></li>
-                <li><a class="nav-link" href="modules-weather-icon.html">Weather Icon</a></li>
-            </ul>
-            </li>
-            <li class="menu-header">Pages</li>
-            <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Auth</span></a>
-            <ul class="dropdown-menu">
-                <li><a href="auth-forgot-password.html">Forgot Password</a></li>
-                <li><a href="auth-login.html">Login</a></li>
-                <li><a class="beep beep-sidebar" href="auth-login-2.html">Login 2</a></li>
-                <li><a href="auth-register.html">Register</a></li>
-                <li><a href="auth-reset-password.html">Reset Password</a></li>
-            </ul>
-            </li>
-            <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-exclamation"></i> <span>Errors</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="errors-503.html">503</a></li>
-                <li><a class="nav-link" href="errors-403.html">403</a></li>
-                <li><a class="nav-link" href="errors-404.html">404</a></li>
-                <li><a class="nav-link" href="errors-500.html">500</a></li>
-            </ul>
-            </li>
-            <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i> <span>Features</span></a>
-            <ul class="dropdown-menu">
-                <li><a class="nav-link" href="features-activities.html">Activities</a></li>
-                <li><a class="nav-link" href="features-post-create.html">Post Create</a></li>
-                <li><a class="nav-link" href="features-posts.html">Posts</a></li>
-                <li><a class="nav-link" href="features-profile.html">Profile</a></li>
-                <li><a class="nav-link" href="features-settings.html">Settings</a></li>
-                <li><a class="nav-link" href="features-setting-detail.html">Setting Detail</a></li>
-                <li><a class="nav-link" href="features-tickets.html">Tickets</a></li>
-            </ul>
-            </li>
-            <li class="nav-item dropdown">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Utilities</span></a>
-            <ul class="dropdown-menu">
-                <li><a href="utilities-contact.html">Contact</a></li>
-                <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
-                <li><a href="utilities-subscribe.html">Subscribe</a></li>
-            </ul>
-            </li>
-            <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Credits</span></a></li>
         </ul>
 
+        {{-- Thanks --}}
         <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-            <i class="fas fa-rocket"></i> Documentation
+            <a
+                href="https://getstisla.com/docs"
+                class="btn btn-danger btn-lg btn-block btn-icon-split">
+                Thanks ❤
             </a>
         </div>
     </aside>
