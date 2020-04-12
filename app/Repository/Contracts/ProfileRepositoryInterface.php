d9b39172bad887fd2d3fbf695ff1396767acefc0
Mourad Bougarne <contact@mbougarne.me>
2020-04-12 17:01:21 +0100

    Remode delete method from Profile Repository

--- app/Repository/Contracts/ProfileRepositoryInterface.php
+++ app/Repository/Contracts/ProfileRepositoryInterface.php
@@ -29,12 +29,4 @@ interface ProfileRepositoryInterface
      * @return \Illuminate\Database\Eloquent\Model|Boolean
      */
     public function update(Profile $profile, array $data);
-
-    /**
-     * Delete a Profile
-     *
-     * @param \App\Models\Profile $profile
-     * @return \Illuminate\Database\Eloquent\Model|Boolean
-     */
-    public function delete(Profile $profile);
 }
