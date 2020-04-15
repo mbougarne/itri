0931c0fca3dbc5f435d1726a7c006523e3333f13
Mourad Bougarne <contact@mbougarne.me>
2020-04-15 18:59:22 +0100

    Feat: Add fetching methods in Post Repository Add latest posts based on status is published or not Add method to get all posts where custome queries Add get all posts by published status

--- app/Repository/Contracts/PostRepositoryInterface.php
+++ app/Repository/Contracts/PostRepositoryInterface.php
@@ -14,6 +14,35 @@ interface PostRepositoryInterface
     public function all();
 
     /**
+     * Get latest posts based on their published status
+     *
+     * @param int $status
+     * @param int $limit number of posts by default 10
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function getLatest(int $status, int $limit = 10);
+
+    /**
+     * Get all posts using custom queries
+     * We have by default all posts sorted by created_at in descending way
+     * For that we use this method in contrary to what we have by default
+     *
+     * @param integer $status published by default
+     * @param string $order_by updated_at by default
+     * @param string $order in ASC  by default
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function allWhere(int $status = 1, string $order_by = 'updated_at', string $order = 'ASC');
+
+    /**
+     * Get all posts where is_published is 1 by default
+     *
+     * @param integer $status
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function allByStatus(int $status = 1);
+
+    /**
      * Get chunck of posts
      *
      * @param integer $limit number of posts
