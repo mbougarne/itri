d26015cc9572152f99d3ec1a8feb45b56922699a
Mourad Bougarne <contact@mbougarne.me>
2020-04-23 15:13:26 +0000

    Refa: Comment Model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,26 +3,58 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\CreatedAtScope;
 
 class Comment extends Model
 {
+    /**
+     * Allow mass assignments
+     * @var array
+    */
     protected $guarded = [];
 
-    public function scopeReply($query)
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    public function boot()
     {
-        return $query->where('is_reply', 1);
+        parent::boot();
+
+        static::addGlobalScope(new CreatedAtScope);
     }
 
-    public function scopeApproved($query)
+    /**
+     * Scope is approved attribute
+     *
+     * @param \Illuminate\Database\Query\Builder $query
+     * @param int $status
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeApproved($query, $status)
     {
-        return $query->where('is_approved', 1);
+        return $query->where('is_approved', $status);
     }
 
-    public function scopePending($query)
+    /**
+     * Scope is subscribed attribute
+     *
+     * @param \Illuminate\Database\Query\Builder $query
+     * @param int $status
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeSubscribed($query, $status)
     {
-        return $query->where('is_approved', 1);
+        return $query->where('is_subscribed', $status);
     }
 
+    /**
+     * Get is Approved attribute
+     *
+     * @param mixed $attribute
+     * @return void
+     */
     public function getIsApprovedAttribute($attribute)
     {
         return [
@@ -31,6 +63,12 @@ class Comment extends Model
         ][$attribute];
     }
 
+    /**
+     * Get is subscribed attribute
+     *
+     * @param mixed $attribute
+     * @return void
+     */
     public function getIsSubscribedAttribute($attribute)
     {
         return [
@@ -39,18 +77,88 @@ class Comment extends Model
         ][$attribute];
     }
 
+    /**
+     * Trim and convert first name to lower case before save
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setFirstNameAttribute($value)
+    {
+        $this->attributes['first_name'] = trim(strtolower($value));
+    }
+
+    /**
+     * Trim and convert last name to lower case before save
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setLastNameAttribute($value)
+    {
+        $this->attributes['last_name'] = trim(strtolower($value));
+    }
+
+    /**
+     * Trim and and replace anchor tags for body before save
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setBodyAttribute($value)
+    {
+        $this->attributes['body'] = trim(str_replace(['<a>', '</a>'], " ", $value));
+    }
+
+    /**
+     * Convert first name to capitalize case
+     *
+     * @param string $value
+     * @return string $value First name
+     */
+    public function getFirstNameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+    /**
+     * Convert last name to capitalize case
+     *
+     * @param string $value
+     * @return string $value First name
+     */
+    public function getLastNameAttribute($value)
+    {
+        return ucfirst($value);
+    }
+
+    /**
+     * Comment Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function post()
     {
         return $this->belongsTo(Post::class);
     }
 
+    /**
+     * Comment Parent
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function parent()
     {
-        return $this->belongsTo(Comment::class);
+        return $this->belongsTo(Comment::class, 'parent_id');
     }
 
+    /**
+     * Comment children
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function children()
     {
-        return $this->HasMany(Comment::class);
+        return $this->HasMany(Comment::class, 'parent_id');
     }
 }
