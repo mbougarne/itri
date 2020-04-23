1b55e2788be78a31ff62e707b5baa8484159cb3b
Mourad Bougarne <contact@mbougarne.me>
2020-04-23 13:40:21 +0000

    Test: Add comment testing

---
+++ tests/Feature/CommentTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CommentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function can_create_comment()
+    {
+        $responsoe = $this->postJson('/comments/create', [
+            'post_id' => 1,
+            'first_name' => 'Mourad',
+            'last_name' => 'Bougarne',
+            'email' => 'contact@mbougarne.me',
+            'body' => 'Just testing the comment',
+        ]);
+
+        $responsoe->assertCreated();
+        $responsoe->dump();
+    }
+}
